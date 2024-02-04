c866f23b10e1c7f349bf9ee9a24a23e4febf9979
Abdullah Kardaş <krdsabdllh53@gmail.com>
2024-02-04 15:26:42 +0100

    updated

--- googleCloud/build.gradle.kts
+++ googleCloud/build.gradle.kts
@@ -3,6 +3,7 @@ plugins {
     alias(libs.plugins.androidLibrary)
     alias(libs.plugins.kotlinAndroid)
     id("org.jetbrains.kotlin.plugin.serialization")
+    `maven-publish`
 }
 
 android {
@@ -67,4 +68,34 @@ dependencies {
     testImplementation(libs.junit)
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
+}
+
+// NOTE : Delete afterEvaluate code if you publish Native Java / Kotlin Library
+// NOTE : Just Using publishing function
+afterEvaluate {
+    publishing {
+        publications {
+
+            // Creates a Maven publication called "release".
+            register("release", MavenPublication::class) {
+
+                // Applies the component for the release build variant.
+                // NOTE : Delete this line code if you publish Native Java / Kotlin Library
+                from(components["release"])
+
+                // Library Package Name (Example : "com.frogobox.androidfirstlib")
+                // NOTE : Different GroupId For Each Library / Module, So That Each Library Is Not Overwritten
+                groupId = "com.akardas16.googlecloud"
+
+                // Library Name / Module Name (Example : "androidfirstlib")
+                // NOTE : Different ArtifactId For Each Library / Module, So That Each Library Is Not Overwritten
+                artifactId = "googlecloud"
+
+                // Version Library Name (Example : "1.0.0")
+                version = "1.0.0"
+
+            }
+
+        }
+    }
 }
